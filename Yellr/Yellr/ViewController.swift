fbc2d052b43d95a61c7ba416923aaba6dbe9f13f
Debjit Saha <debjitsaha87@gmail.com>
2015-05-26 14:03:09 -0400

    Initial Commit

---
+++ Yellr/Yellr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yellr
+//
+//  Created by Debjit Saha on 5/26/15.
+//  Copyright (c) 2015 wxxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
