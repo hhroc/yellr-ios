0837e2eb1e52878bead98c0f5974f1e33f72751e
Debjit Saha <debjitsaha87@gmail.com>
2015-05-29 11:12:47 -0400

    Use UISegmentedControls for Top Tab views (no Customisation as of now)

--- Yellr/Yellr/ViewController.swift
+++ Yellr/Yellr/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var view1: UITableView!
+    @IBOutlet weak var view2: UITableView!
+    @IBOutlet weak var view3: UITableView!
+
+    @IBOutlet weak var topTab: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view2.hidden = true;
+        self.view3.hidden = true;
+        self.topTab.setEnabled(true, forSegmentAtIndex: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tabSelected(sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex == 0) {
+            self.view1.hidden = false;
+            self.view2.hidden = true;
+            self.view3.hidden = true;
+        } else if (sender.selectedSegmentIndex == 1) {
+            self.view1.hidden = true;
+            self.view2.hidden = false;
+            self.view3.hidden = true;
+        } else if (sender.selectedSegmentIndex == 2) {
+            self.view1.hidden = true;
+            self.view2.hidden = true;
+            self.view3.hidden = false;
+        }
+    }
+    
 
 }
 
