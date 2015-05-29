b0b0da03b065778aa09bba323e79d15fa8427951
Debjit Saha <debjitsaha87@gmail.com>
2015-05-29 15:41:42 -0400

    VC for the Local Posts Screen - using static data source

--- Yellr/Yellr/LocalTableViewController.swift
+++ Yellr/Yellr/LocalTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  LocalTableViewController.swift
 //  Yellr
 //
 //  Created by Debjit Saha on 5/29/15.
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class LocalTableViewController: UITableViewController {
+    
+    var data = ["Contrary to popular belief", "Lorem Ipsum is not", "simply random text", "It has roots", "in a piece of", "classical Latin", "literature from", "45 BC, making it", "over 2000 years old", "Richard McClintock", "a Latin professor", "at Hampden-Sydney College", "in Virginia, looked up", "one of the more obscure", "Latin words, consectetur", "from a Lorem Ipsum", "passage, and going", "through the cites", "of the word in", "classical literature", "discovered the", "undoubtable source"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
-
-
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("LocalTVCIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
