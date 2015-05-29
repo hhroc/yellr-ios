f599c489d64741de7d661ec4dcb4d5db53bab4db
Debjit Saha <debjitsaha87@gmail.com>
2015-05-29 18:36:43 -0400

    Update view / scene structure

--- Yellr/Yellr/AssignmentsTableViewController.swift
+++ Yellr/Yellr/AssignmentsTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class AssignmentsTableViewController: UITableViewController {
     
+    //
     var data = ["There are many variations", "of passages of Lorem", "Ipsum available, but", "the majority have suffered", "alteration in some form", "by injected humour", "or randomised words", "which don't look even", "slightly believable. If", "you are going to use", "a passage of Lorem Ipsum", "you need to be sure", "there isn't anything", "embarrassing hidden in", "the middle of text", "All the Lorem Ipsum", "generators on the Internet", "tend to repeat predefined", "chunks as necessary"]
     
     override func viewDidLoad() {
--- Yellr/Yellr/LocalTableViewController.swift
+++ Yellr/Yellr/LocalTableViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class LocalTableViewController: UITableViewController {
     
+    //This will be replaced by an actual data source
+    //from the API response
     var data = ["Contrary to popular belief", "Lorem Ipsum is not", "simply random text", "It has roots", "in a piece of", "classical Latin", "literature from", "45 BC, making it", "over 2000 years old", "Richard McClintock", "a Latin professor", "at Hampden-Sydney College", "in Virginia, looked up", "one of the more obscure", "Latin words, consectetur", "from a Lorem Ipsum", "passage, and going", "through the cites", "of the word in", "classical literature", "discovered the", "undoubtable source"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    //Determine the number of rows to show
+    //in the tableview
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
     
+    //Update the cell object to show labels
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("LocalTVCIdentifier", forIndexPath: indexPath) as! UITableViewCell
         cell.textLabel?.text = data[indexPath.row]
