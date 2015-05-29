5643a5635b66d4036e1da71e606a0fecef468d6f
Debjit Saha <debjitsaha87@gmail.com>
2015-05-29 15:46:50 -0400

    VC for the Assignments Tab

--- Yellr/Yellr/AssignmentsTableViewController.swift
+++ Yellr/Yellr/AssignmentsTableViewController.swift
@@ -6,4 +6,25 @@
 //  Copyright (c) 2015 wxxi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AssignmentsTableViewController: UITableViewController {
+    
+    var data = ["There are many variations", "of passages of Lorem", "Ipsum available, but", "the majority have suffered", "alteration in some form", "by injected humour", "or randomised words", "which don't look even", "slightly believable. If", "you are going to use", "a passage of Lorem Ipsum", "you need to be sure", "there isn't anything", "embarrassing hidden in", "the middle of text", "All the Lorem Ipsum", "generators on the Internet", "tend to repeat predefined", "chunks as necessary"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AssignmentsTVCIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    
+}
