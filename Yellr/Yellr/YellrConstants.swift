409d6244936c6bb8c165d2cfe3f79e19716ff618
Debjit Saha <debjitsaha87@gmail.com>
2015-06-01 13:28:34 -0400

    Add a global constants file for Yellr

--- Yellr/Yellr/YellrConstants.swift
+++ Yellr/Yellr/YellrConstants.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct YellrConstants {
+    struct LocalPosts {
+        static let Title = "local_post_title"
+    }
+    
+    struct Assignments {
+        static let Title = "assignments_title"
+    }
+    
+    struct Stories {
+        static let Title = "stories_title"
+    }
+    
+    struct Profile {
+        static let Title = "profile_title"
+    }
+    
+    struct NewPost {
+        static let Title = "new_post_title"
+    }
+}
