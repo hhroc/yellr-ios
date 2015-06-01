0bca27d081f12e34df5ec20c44ece92ba4938310
Debjit Saha <debjitsaha87@gmail.com>
2015-06-01 16:15:23 -0400

    Splash Screen Updated

--- Yellr/Yellr/YellrConstants.swift
+++ Yellr/Yellr/YellrConstants.swift
@@ -28,4 +28,22 @@ struct YellrConstants {
     struct NewPost {
         static let Title = "new_post_title"
     }
+    
+    struct Colors {
+        static let blue = "#2980b9";
+        static let dark_blue = "#2c3e50";
+        static let yellow = "#FFD40C";
+        static let dark_yellow = "#F2BF00";
+        static let light_yellow = "#FFF22A";
+        static let white = "#ffffff";
+        static let black = "#2f2f2f";
+        static let red = "#ff6347";
+        static let down_vote_red = "#ff6347";
+        static let green = "#1abc9c";
+        static let up_vote_green = "#1abc9c";
+        static let grey = "#444";
+        static let light_grey = "#bbb";
+        static let assignment_response_grey = "#e0e0e0";
+        static let background = "#eee";
+    }
 }
