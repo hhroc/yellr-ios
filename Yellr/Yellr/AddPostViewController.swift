a3c713b61a255a8088efe45ffb4f96a9f8903050
Debjit Saha <debjitsaha87@gmail.com>
2015-06-02 19:26:03 -0400

    Add FontAwesome to the App

--- Yellr/Yellr/AddPostViewController.swift
+++ Yellr/Yellr/AddPostViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class AddPostViewController: UIViewController {
     
+    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var photoBtn: UIButton!
+    @IBOutlet weak var vdoBtn: UIButton!
+    @IBOutlet weak var recordBtn: UIButton!
+    @IBOutlet weak var submitBtn: UIBarButtonItem!
+    @IBOutlet weak var cancelBtn: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = NSLocalizedString(YellrConstants.AddPost.Title, comment: "Add Post Screen title")
+        
+        var buttonString = String.fontAwesomeString("fa-camera")
+        var buttonStringAttributed = NSMutableAttributedString(string: buttonString, attributes: [NSFontAttributeName:UIFont(name: "HelveticaNeue", size: 11.00)!])
+        buttonStringAttributed.addAttribute(NSFontAttributeName, value: UIFont.iconFontOfSize("FontAwesome", fontSize: 50), range: NSRange(location: 0,length: 1))
+        
+        photoBtn.titleLabel?.textAlignment = .Center
+        photoBtn.titleLabel?.numberOfLines = 2
+        photoBtn.setAttributedTitle(buttonStringAttributed, forState: .Normal)
+        
     }
     
 }
