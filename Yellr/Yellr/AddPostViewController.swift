9bfef17daff22f5f649d410b59b308b5c88c321b
Debjit Saha <debjitsaha87@gmail.com>
2015-06-11 22:44:49 -0400

    Add iOS version check for label location hud

--- Yellr/Yellr/AddPostViewController.swift
+++ Yellr/Yellr/AddPostViewController.swift
@@ -65,19 +65,19 @@ class AddPostViewController: UIViewController {
         spinningActivity.labelText = "Posting"
         spinningActivity.userInteractionEnabled = false
         
+        var postFail : Bool = false
         var postCont = postContent.text
-        postingIndicator.hidden = false
+
         post(["media_type":"text", "media_file":"text", "media_text":postCont], "upload_media") { (succeeded: Bool, msg: String) -> () in
             println("Media Uploaded : " + msg)
-            if (msg != "NOTHING") {
+            if (msg != "NOTHING" && msg != "Error") {
                 post(["assignment_id":"0", "media_objects":"[\""+msg+"\"]"], "publish_post") { (succeeded: Bool, msg: String) -> () in
                     println("Post Added : " + msg)
                     if (msg != "NOTHING") {
 
                         self.dismissViewControllerAnimated(true, completion: nil);
-                        self.postingIndicator.hidden = true
                         MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
-                        
+                        postFail = true
                         //show done hud on the source view controller
                         dispatch_async(dispatch_get_main_queue()) {
                             let spinningActivityDone = MBProgressHUD.showHUDAddedTo(self.presentingViewController?.view, animated: true)
@@ -85,14 +85,26 @@ class AddPostViewController: UIViewController {
                             spinningActivityDone.customView = UIImageView(image: checkImage)
                             spinningActivityDone.mode = MBProgressHUDMode.CustomView
                             spinningActivityDone.labelText = NSLocalizedString(YellrConstants.AddPost.SuccessMsg, comment: "Add Post Success")
-                            spinningActivityDone.hide(true, afterDelay: NSTimeInterval(1))
+                            spinningActivityDone.hide(true, afterDelay: NSTimeInterval(3))
                         }
                         
                     } else {
                         //fail toast
-                        self.postingIndicator.hidden = true
-                        MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
+                        postFail = true
                     }
+                }
+            } else {
+                postFail = true
+            }
+            if (postFail) {
+                dispatch_async(dispatch_get_main_queue()) {
+                    MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
+                    let spinningActivityFail = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+                    spinningActivityFail.customView = UIView()
+                    spinningActivityFail.mode = MBProgressHUDMode.CustomView
+                    spinningActivityFail.labelText = NSLocalizedString(YellrConstants.AddPost.FailMsg, comment: "Add Post Fail")
+                    spinningActivityFail.yOffset = iOS8 ? 225 : 175
+                    spinningActivityFail.hide(true, afterDelay: NSTimeInterval(2.5))
                 }
             }
         }
